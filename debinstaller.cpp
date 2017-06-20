2a565097f568bf3d782b8773d4af063474ae884e
石博文 <sbw@sbw.so>
2017-06-20 16:03:24 +0800

    add command line arguments

--- debinstaller.cpp
+++ debinstaller.cpp
@@ -54,9 +54,21 @@ void DebInstaller::onPackagesSelected(const QStringList &packages)
     for (const auto &package : packages)
     {
         DebFile *p = new DebFile(package);
+        if (!p->isValid())
+        {
+            qWarning() << "package invalid: " << p->filePath();
+
+            delete p;
+            continue;
+        }
 
         m_fileListModel->appendPackage(p);
     }
+
+    const int packageCount = m_fileListModel->preparedPackages().size();
+    // no packages found
+    if (packageCount == 0)
+        return;
 
     SingleInstallPage *singlePage = new SingleInstallPage;
     singlePage->setPackage(m_fileListModel->preparedPackages().first());
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "debinstaller.h"
 
 #include <DApplication>
+#include <QCommandLineParser>
+#include <QDebug>
+#include <QTimer>
 
 DWIDGET_USE_NAMESPACE
 
@@ -15,8 +18,24 @@ int main(int argc, char *argv[])
     app.loadTranslator();
     app.setTheme("light");
 
+    // command line arguments
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Deepin deb package installer.");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument("filename", QApplication::translate("main", "Deb package path."), "file [file..]");
+
+    parser.process(app);
+
+    const QStringList file_list = parser.positionalArguments();
+    qDebug() << file_list;
+
     DebInstaller w;
     w.show();
+
+    // select files from args
+    if (!file_list.isEmpty())
+        QMetaObject::invokeMethod(&w, "onPackagesSelected", Qt::QueuedConnection, Q_ARG(QStringList, file_list));
 
     return app.exec();
 }
