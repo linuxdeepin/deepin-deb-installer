8f23e7d239d5a536b81c815d4328ffef6470f125
石博文 <sbw@sbw.so>
2017-07-12 15:53:22 +0800

    add tx dir
    Change-Id: I6118695145006d6fa8cc86a7c42cde631098ed21

---
+++ translations/deepin-deb-installer.ts
@@ -0,0 +1,136 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>DebInstaller</name>
+    <message>
+        <location filename="../debinstaller.cpp" line="32"/>
+        <source>Deepin Deb Installer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../debinstaller.cpp" line="88"/>
+        <source>Bulk Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DebListModel</name>
+    <message>
+        <location filename="../deblistmodel.cpp" line="222"/>
+        <source>Dependencies Break</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FileChooseWidget</name>
+    <message>
+        <location filename="../filechoosewidget.cpp" line="26"/>
+        <source>Drag and drop files here</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../filechoosewidget.cpp" line="53"/>
+        <source>Choose Package</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InfoControlButton</name>
+    <message>
+        <location filename="../infocontrolbutton.cpp" line="17"/>
+        <location filename="../infocontrolbutton.cpp" line="51"/>
+        <source>Show Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../infocontrolbutton.cpp" line="54"/>
+        <source>Hide Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MultipleInstallPage</name>
+    <message>
+        <location filename="../multipleinstallpage.cpp" line="29"/>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../multipleinstallpage.cpp" line="38"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SingleInstallPage</name>
+    <message>
+        <location filename="../singleinstallpage.cpp" line="84"/>
+        <source>Install</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../singleinstallpage.cpp" line="87"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../singleinstallpage.cpp" line="93"/>
+        <source>Reinstall</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../singleinstallpage.cpp" line="99"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../singleinstallpage.cpp" line="113"/>
+        <source>Package: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../singleinstallpage.cpp" line="120"/>
+        <source>Version: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../singleinstallpage.cpp" line="256"/>
+        <source>Installed successfully</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../singleinstallpage.cpp" line="261"/>
+        <source>Install Failed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../singleinstallpage.cpp" line="307"/>
+        <source>Other version installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../singleinstallpage.cpp" line="309"/>
+        <source>Same version installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../singleinstallpage.cpp" line="318"/>
+        <source>Broken Dependencies</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="22"/>
+        <source>Deepin Package Manager is used to help users install and remove local software, supports to bulk install.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="34"/>
+        <source>Deb package path.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
