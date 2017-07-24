4d61891514481eca6ea56ae78f1e87da74cd2f16
transifex <transifex@linuxdeepin.com>
2017-07-24 02:52:10 +0000

    auto sync po files from transifex
    Change-Id: Ia4b6aa16843aa048a58a0c2be967129f23d4d70c

--- translations/deepin-deb-installer_ar.ts
+++ translations/deepin-deb-installer_ar.ts
@@ -1,15 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ar" version="2.1">
 <context>
     <name>DebInstaller</name>
     <message>
         <location filename="../debinstaller.cpp" line="33"/>
         <source>Deepin Package Manager</source>
-        <translation>深度软件包管理器</translation>
+        <translation>مدير حزم ديبين</translation>
     </message>
     <message>
         <location filename="../debinstaller.cpp" line="91"/>
         <source>Bulk Install</source>
-        <translation>批量安装</translation>
+        <translation>حجم التثبيت</translation>
     </message>
 </context>
 <context>
@@ -17,28 +17,28 @@
     <message>
         <location filename="../deblistmodel.cpp" line="22"/>
         <source>Installation failed, please check your network connection</source>
-        <translation>安装失败，请检查您的网络连接</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../deblistmodel.cpp" line="24"/>
         <source>Installation failed, please check updates in Control Center</source>
-        <translation>安装失败，请在控制中心检查更新</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../deblistmodel.cpp" line="26"/>
         <source>Installation failed, insufficient disk space</source>
-        <translation>安装失败，磁盘空间不足</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../deblistmodel.cpp" line="29"/>
         <source>Installation Failed</source>
-        <translation>安装失败</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../deblistmodel.cpp" line="213"/>
         <location filename="../deblistmodel.cpp" line="217"/>
         <source>Broken Dependencies: %1</source>
-        <translation>依赖关系不满足：%1</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
@@ -46,12 +46,12 @@
     <message>
         <location filename="../filechoosewidget.cpp" line="26"/>
         <source>Drag and drop files here</source>
-        <translation>拖拽软件包到此</translation>
+        <translation>اسحب الملفات وأفلتها هنا</translation>
     </message>
     <message>
         <location filename="../filechoosewidget.cpp" line="53"/>
         <source>Select File</source>
-        <translation>选择文件</translation>
+        <translation>تحديد ملف</translation>
     </message>
 </context>
 <context>
@@ -59,22 +59,22 @@
     <message>
         <location filename="../multipleinstallpage.cpp" line="19"/>
         <source>Display installation process</source>
-        <translation>显示安装进程</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../multipleinstallpage.cpp" line="19"/>
         <source>Collapse</source>
-        <translation>收起</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../multipleinstallpage.cpp" line="31"/>
         <source>Install</source>
-        <translation>安装</translation>
+        <translation>تثبيت</translation>
     </message>
     <message>
         <location filename="../multipleinstallpage.cpp" line="32"/>
         <source>OK</source>
-        <translation>确定</translation>
+        <translation>موافق</translation>
     </message>
 </context>
 <context>
@@ -82,17 +82,17 @@
     <message>
         <location filename="../packageslistdelegate.cpp" line="74"/>
         <source>Installing</source>
-        <translation>正在安装</translation>
+        <translation>يتم التثبيت</translation>
     </message>
     <message>
         <location filename="../packageslistdelegate.cpp" line="78"/>
         <source>Installed</source>
-        <translation>已安装</translation>
+        <translation>مثبت</translation>
     </message>
     <message>
         <location filename="../packageslistdelegate.cpp" line="82"/>
         <source>Failed</source>
-        <translation>安装失败</translation>
+        <translation>فشل</translation>
     </message>
 </context>
 <context>
@@ -100,67 +100,67 @@
     <message>
         <location filename="../singleinstallpage.cpp" line="60"/>
         <source>Display installation process</source>
-        <translation>显示安装进程</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="60"/>
         <source>Collapse</source>
-        <translation>收起</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="88"/>
         <source>Install</source>
-        <translation>安装</translation>
+        <translation>تثبيت</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="90"/>
         <source>Remove</source>
-        <translation>卸载</translation>
+        <translation>إزالة</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="92"/>
         <source>Reinstall</source>
-        <translation>重新安装</translation>
+        <translation>إعادة التثبيت</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="94"/>
         <source>OK</source>
-        <translation>确定</translation>
+        <translation>موافق</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="101"/>
         <source>Package: </source>
-        <translation>软件包：</translation>
+        <translation>الحزمة :</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="108"/>
         <source>Version: </source>
-        <translation>版本：</translation>
+        <translation>الإصدار :</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="250"/>
         <source>Installed successfully</source>
-        <translation>安装成功</translation>
+        <translation>تم التثبيت بنجاح</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="252"/>
         <source>Uninstalled successfully</source>
-        <translation>卸载成功</translation>
+        <translation>تم إلغاء التثبيت بنجاح</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="313"/>
         <source>Other version installed: %1</source>
-        <translation>已安装其他版本：%1</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="260"/>
         <source>Uninstall Failed</source>
-        <translation>卸载失败</translation>
+        <translation>فشل إزالة التثبيت</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="311"/>
         <source>Same version installed</source>
-        <translation>已安装相同版本</translation>
+        <translation>الإصدار نفسه مثبت</translation>
     </message>
 </context>
 <context>
@@ -168,29 +168,28 @@
     <message>
         <location filename="../uninstallconfirmpage.cpp" line="12"/>
         <source>Display related packages</source>
-        <translation>显示相关软件包</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../uninstallconfirmpage.cpp" line="12"/>
         <source>Collapse</source>
-        <translation>收起</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../uninstallconfirmpage.cpp" line="25"/>
         <source>Cancel</source>
-        <translation>取消</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../uninstallconfirmpage.cpp" line="27"/>
         <source>Confirm</source>
-        <translation>确认卸载</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../uninstallconfirmpage.cpp" line="80"/>
         <source>Are you sure to uninstall %1?
 All dependencies will also be removed</source>
-        <translation>您确定要卸载XXX吗？
-所有依赖也会被一起移除</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
@@ -198,7 +197,7 @@ All dependencies will also be removed</source>
     <message>
         <location filename="../main.cpp" line="22"/>
         <source>Deepin Package Manager is used to help users install and remove local software, supports to bulk install.</source>
-        <translation>深度软件包管理器用于帮助用户安装和卸载本地软件，支持批量安装</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 </TS>
--- translations/deepin-deb-installer_da.ts
+++ translations/deepin-deb-installer_da.ts
@@ -1,15 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="da" version="2.1">
 <context>
     <name>DebInstaller</name>
     <message>
         <location filename="../debinstaller.cpp" line="33"/>
         <source>Deepin Package Manager</source>
-        <translation>深度软件包管理器</translation>
+        <translation>Deepin pakkehåndtering</translation>
     </message>
     <message>
         <location filename="../debinstaller.cpp" line="91"/>
         <source>Bulk Install</source>
-        <translation>批量安装</translation>
+        <translation>Masseinstallation</translation>
     </message>
 </context>
 <context>
@@ -17,28 +17,28 @@
     <message>
         <location filename="../deblistmodel.cpp" line="22"/>
         <source>Installation failed, please check your network connection</source>
-        <translation>安装失败，请检查您的网络连接</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../deblistmodel.cpp" line="24"/>
         <source>Installation failed, please check updates in Control Center</source>
-        <translation>安装失败，请在控制中心检查更新</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../deblistmodel.cpp" line="26"/>
         <source>Installation failed, insufficient disk space</source>
-        <translation>安装失败，磁盘空间不足</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../deblistmodel.cpp" line="29"/>
         <source>Installation Failed</source>
-        <translation>安装失败</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../deblistmodel.cpp" line="213"/>
         <location filename="../deblistmodel.cpp" line="217"/>
         <source>Broken Dependencies: %1</source>
-        <translation>依赖关系不满足：%1</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
@@ -46,12 +46,12 @@
     <message>
         <location filename="../filechoosewidget.cpp" line="26"/>
         <source>Drag and drop files here</source>
-        <translation>拖拽软件包到此</translation>
+        <translation>Træk-og-slip filer her</translation>
     </message>
     <message>
         <location filename="../filechoosewidget.cpp" line="53"/>
         <source>Select File</source>
-        <translation>选择文件</translation>
+        <translation>Vælg fil</translation>
     </message>
 </context>
 <context>
@@ -59,22 +59,22 @@
     <message>
         <location filename="../multipleinstallpage.cpp" line="19"/>
         <source>Display installation process</source>
-        <translation>显示安装进程</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../multipleinstallpage.cpp" line="19"/>
         <source>Collapse</source>
-        <translation>收起</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../multipleinstallpage.cpp" line="31"/>
         <source>Install</source>
-        <translation>安装</translation>
+        <translation>Installer</translation>
     </message>
     <message>
         <location filename="../multipleinstallpage.cpp" line="32"/>
         <source>OK</source>
-        <translation>确定</translation>
+        <translation>OK</translation>
     </message>
 </context>
 <context>
@@ -82,17 +82,17 @@
     <message>
         <location filename="../packageslistdelegate.cpp" line="74"/>
         <source>Installing</source>
-        <translation>正在安装</translation>
+        <translation>Installerer</translation>
     </message>
     <message>
         <location filename="../packageslistdelegate.cpp" line="78"/>
         <source>Installed</source>
-        <translation>已安装</translation>
+        <translation>Installeret</translation>
     </message>
     <message>
         <location filename="../packageslistdelegate.cpp" line="82"/>
         <source>Failed</source>
-        <translation>安装失败</translation>
+        <translation>Mislykkedes</translation>
     </message>
 </context>
 <context>
@@ -100,67 +100,67 @@
     <message>
         <location filename="../singleinstallpage.cpp" line="60"/>
         <source>Display installation process</source>
-        <translation>显示安装进程</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="60"/>
         <source>Collapse</source>
-        <translation>收起</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="88"/>
         <source>Install</source>
-        <translation>安装</translation>
+        <translation>Installer</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="90"/>
         <source>Remove</source>
-        <translation>卸载</translation>
+        <translation>Fjern</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="92"/>
         <source>Reinstall</source>
-        <translation>重新安装</translation>
+        <translation>Geninstaller</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="94"/>
         <source>OK</source>
-        <translation>确定</translation>
+        <translation>OK</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="101"/>
         <source>Package: </source>
-        <translation>软件包：</translation>
+        <translation>Pakke:</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="108"/>
         <source>Version: </source>
-        <translation>版本：</translation>
+        <translation>Version:</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="250"/>
         <source>Installed successfully</source>
-        <translation>安装成功</translation>
+        <translation>Installeret</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="252"/>
         <source>Uninstalled successfully</source>
-        <translation>卸载成功</translation>
+        <translation>Afinstalleret</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="313"/>
         <source>Other version installed: %1</source>
-        <translation>已安装其他版本：%1</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="260"/>
         <source>Uninstall Failed</source>
-        <translation>卸载失败</translation>
+        <translation>Afinstallation mislykkedes</translation>
     </message>
     <message>
         <location filename="../singleinstallpage.cpp" line="311"/>
         <source>Same version installed</source>
-        <translation>已安装相同版本</translation>
+        <translation>Same version installeret</translation>
     </message>
 </context>
 <context>
@@ -168,29 +168,28 @@
     <message>
         <location filename="../uninstallconfirmpage.cpp" line="12"/>
         <source>Display related packages</source>
-        <translation>显示相关软件包</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../uninstallconfirmpage.cpp" line="12"/>
         <source>Collapse</source>
-        <translation>收起</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../uninstallconfirmpage.cpp" line="25"/>
         <source>Cancel</source>
-        <translation>取消</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../uninstallconfirmpage.cpp" line="27"/>
         <source>Confirm</source>
-        <translation>确认卸载</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../uninstallconfirmpage.cpp" line="80"/>
         <source>Are you sure to uninstall %1?
 All dependencies will also be removed</source>
-        <translation>您确定要卸载XXX吗？
-所有依赖也会被一起移除</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
@@ -198,7 +197,7 @@ All dependencies will also be removed</source>
     <message>
         <location filename="../main.cpp" line="22"/>
         <source>Deepin Package Manager is used to help users install and remove local software, supports to bulk install.</source>
-        <translation>深度软件包管理器用于帮助用户安装和卸载本地软件，支持批量安装</translation>
+        <translation>Deepin pakkehåndtering bruges til at hjælpe brugere med at installere og fjerne lokal software, med understøttelse af masseinstallation.</translation>
     </message>
 </context>
 </TS>
