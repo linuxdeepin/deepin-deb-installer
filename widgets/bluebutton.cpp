909df589271b104b6c625fc09531474fdb73ae2d
石博文 <sbw@sbw.so>
2017-08-03 19:41:39 +0800

    add hover and pressed linear gradient to buttons
    Change-Id: Icc0b9e7b6591d71ce02a9ca6b60dfeb62a0d7014

--- widgets/bluebutton.cpp
+++ widgets/bluebutton.cpp
@@ -8,5 +8,14 @@ BlueButton::BlueButton(QWidget *parent)
                   "color: #2ca7f8;"
                   "border: 1px solid #2ca7f8;"
                   "border-radius: 4px;"
+                  "}"
+                  ""
+                  "BlueButton:hover {"
+                  "color: white;"
+                  "background-color: qlineargradient(x1:0 y1:0, x2:0 y2:1, stop:0 #8ccfff, stop:1 #4bb8ff);"
+                  "}"
+                  ""
+                  "BlueButton:pressed {"
+                  "background-color: qlineargradient(x1:0 y1:0, x2:0 y2:1, stop:0 #0b8cff, stop:1 #0aa1ff);"
                   "}");
 }
