06913cd1456f36380ed9d3c3436b0cef4eebe158
cuizhen <cuizhen@uniontech.com>
2020-08-11 10:48:27 +0800

    fix: 更新翻译
    更新翻译
    
    Log: 更新翻译
    
    link: 无

--- translations/deepin-deb-installer_zh_CN.ts
+++ translations/deepin-deb-installer_zh_CN.ts
@@ -45,6 +45,11 @@
         <translation>安装失败</translation>
     </message>
     <message>
+        <location filename="../src/deb_installer/deblistmodel.cpp" line="471"/>
+        <source>Failed to install %1</source>
+        <translation>%1 下载失败</translation>
+    </message>
+    <message>
         <location filename="../src/deb_installer/deblistmodel.cpp" line="653"/>
         <source>Unable to install</source>
         <translation>无法安装</translation>
@@ -58,11 +63,6 @@
         <location filename="../src/deb_installer/deblistmodel.cpp" line="656"/>
         <source>OK</source>
         <translation>确定</translation>
-    </message>
-    <message>
-        <location filename="../src/deb_installer/deblistmodel.cpp" line="471"/>
-        <source>%1 Installation Failed</source>
-        <translation>%1 安装失败</translation>
     </message>
     <message>
         <location filename="../src/deb_installer/deblistmodel.cpp" line="472"/>
@@ -230,8 +230,8 @@
     </message>
     <message>
         <location filename="../src/deb_installer/singleinstallpage.cpp" line="848"/>
-        <source>%1 Installation Failed</source>
-        <translation>%1 安装失败</translation>
+        <source>Failed to install %1</source>
+        <translation>%1 下载失败</translation>
     </message>
     <message>
         <location filename="../src/deb_installer/singleinstallpage.cpp" line="172"/>
