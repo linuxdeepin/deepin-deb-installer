1a9f16475ed34a7bea40f688e7e1d1e8f31dbc5a
cuizhen <cuizhen@uniontech.com>
2020-08-20 10:42:53 +0800

    fix: 修复多包安装时，安装失败，错误原因字体样式显示错误的问题。
    1. 删除未选中时设置错误原因字体样式为TextTip
    
    Log: 修复多包安装时，安装失败，错误原因字体样式显示错误的问题。
    
    link: 无

--- src/deb_installer/packageslistdelegate.cpp
+++ src/deb_installer/packageslistdelegate.cpp
@@ -256,9 +256,6 @@ void PackagesListDelegate::paint(QPainter *painter, const QStyleOptionViewItem &
         if (option.state & DStyle::State_Enabled) {
             if (option.state & DStyle::State_Selected) {
                 forground.setColor(palette.color(cg, DPalette::HighlightedText));
-            } else {
-                //fix bug: 43139
-                forground.setColor(palette.color(cg, DPalette::TextTips));
             }
         }
         painter->setPen(forground);
