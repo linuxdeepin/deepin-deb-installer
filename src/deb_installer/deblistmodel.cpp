200a5cc684be50c22ade69da9d6c9f3d13adf050
cuizhen <cuizhen@uniontech.com>
2020-08-27 09:54:34 +0800

    fix: 修复弹出窗口无焦点的问题
    1.增加签名验证失败弹出窗口的按钮焦点
    
    Log: 无有效签名的提示框无法通过tab键被选中
    
    link: https://pms.uniontech.com/zentao/bug-view-44837.html

--- src/deb_installer/deblistmodel.cpp
+++ src/deb_installer/deblistmodel.cpp
@@ -681,6 +681,8 @@ void DebListModel::showNoDigitalErrWindow()
     Ddialog->addButton(QString(tr("OK")), true, DDialog::ButtonNormal);
     Ddialog->show();
     QPushButton *btnOK = qobject_cast<QPushButton *>(Ddialog->getButton(0));
+    btnOK->setFocusPolicy(Qt::WheelFocus);
+    btnOK->setFocus();
     connect(Ddialog, &DDialog::aboutToClose, this, [ = ] {
         if (preparedPackages().size() > 1)
         {
