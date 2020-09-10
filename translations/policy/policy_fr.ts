f7b61f27fac431cccf547ce0349bbcbaf23ef9f7
cuizhen <cuizhen@uniontech.com>
2020-09-10 11:08:43 +0800

    fix: 修改desktop以及更新翻译文件
    1.修改desktop
    2.更新翻译文件
    
    Log:  【sp3】【软件包安装器】【5.6.0.17】正体中文_TW、繁体中文_HK两种语言下，存在界面未翻译问题
    
    link: null
    Change-Id: I64ac1550529e8d56df0df0d47ce112277cedddbd
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/deepin-deb-installer/+/4235
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: luzhou <luzhou@uniontech.com>
    Tested-by: <mailman@uniontech.com>

---
+++ translations/policy/policy_fr.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.1">
+	<context>
+		<name>policy</name>
+		<message>
+			<location filename="com.deepin.pkexec.deepin-deb-installer-dependsInstall!message" line="0"/>
+			<source>Authentication is required to continue the configuration</source>
+			<translation>L&apos;authentification est requise pour continuer la configuration</translation>
+		</message>
+		<message>
+			<location filename="com.deepin.pkexec.deepin-deb-installer-dependsInstall!description" line="0"/>
+			<source>authentication</source>
+			<translation>authentification</translation>
+		</message>
+	</context>
+</TS>
