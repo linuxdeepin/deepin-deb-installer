2f25c0edab73b55ce9b4c38f78f2d552ad746c6f
cuizhen <cuizhen@uniontech.com>
2020-09-16 19:12:15 +0800

    fix: 解决应用打开不在桌面正中心，没有帮助
    1.解决应用打开不在桌面正中心
    2.解决应用没有帮助等问题
    
    Log: 解决应用打开不在桌面正中心，没有帮助
    Change-Id: Iccbd516f86849702fccced07b5f6c63d1a861eb3
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/deepin-deb-installer/+/5142
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: cuizhen <cuizhen@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- main.cpp
+++ main.cpp
@@ -49,6 +49,7 @@ int main(int argc, char *argv[])
 {
     //DApplication::loadDXcbPlugin();
 
+    qputenv("QT_WAYLAND_SHELL_INTEGRATION", "kwayland-shell");
     DApplication app(argc, argv);
 
     app.setOrganizationName("deepin");
