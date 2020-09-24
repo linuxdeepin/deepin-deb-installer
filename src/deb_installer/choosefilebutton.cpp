1fd0546e6d7fc5cad9f89256ba5dba69b56d5585
cuizhen <cuizhen@uniontech.com>
2020-09-24 10:14:00 +0800

    chore: 添加chooseFileButton代码注释
    添加chooseFileButton代码注释
    
    Log: 添加chooseFileButton代码注释
    Change-Id: I50b0b5277853d6caa4b6d11ed142a9fd10110ebc
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/deepin-deb-installer/+/6097
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: liujun <liujuna@uniontech.com>
    Reviewed-by: cuizhen <cuizhen@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/deb_installer/choosefilebutton.cpp
+++ src/deb_installer/choosefilebutton.cpp
@@ -8,10 +8,12 @@
 ChooseFileButton::ChooseFileButton(QString text, QWidget *parent)
     : DCommandLinkButton(text, parent)
 {
-    QString fontFamily = Utils::loadFontFamilyByType(Utils::SourceHanSansNormal);
-    Utils::bindFontBySizeAndWeight(this, fontFamily, 12, QFont::ExtraLight);
+    QString fontFamily = Utils::loadFontFamilyByType(Utils::SourceHanSansNormal);   //设置字体
+    Utils::bindFontBySizeAndWeight(this, fontFamily, 12, QFont::ExtraLight);       //设置字体样式与字体大小
 
-    this->setFocusPolicy(Qt::TabFocus);
+    this->setFocusPolicy(Qt::TabFocus);                                            //设置本身可以被焦点选中
+
+    // 添加AccessibleName
     this->setObjectName("ChooseFileButton");
     this->setAccessibleName("ChooseFileButton");
 }
@@ -22,6 +24,7 @@ ChooseFileButton::ChooseFileButton(QString text, QWidget *parent)
  */
 void ChooseFileButton::keyPressEvent(QKeyEvent *event)
 {
+    // 当按下回车、换行、或空格时，如果当前按钮存在焦点则触发click事件
     if (event->key() == Qt::Key_Return || event->key() == Qt::Key_Space || event->key() == Qt::Key_Enter) {
         if (this->hasFocus()) {
             this->clicked();
