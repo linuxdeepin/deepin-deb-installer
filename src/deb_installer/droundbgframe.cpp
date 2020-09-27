4801d1fa49db9e283ffac764b950de935e525f85
cuizhen <cuizhen@uniontech.com>
2020-09-27 17:21:44 +0800

    chore: 添加droundbgFrame注释
    添加droundbgFrame注释
    
    Log: 添加droundbgFrame注释
    Change-Id: I87fc6998ac4a50d77a2f9948ba49be912c8332cf
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/deepin-deb-installer/+/6452
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: liujun <liujuna@uniontech.com>
    Reviewed-by: cuizhen <cuizhen@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/deb_installer/droundbgframe.cpp
+++ src/deb_installer/droundbgframe.cpp
@@ -13,25 +13,30 @@ DRoundBgFrame::DRoundBgFrame(QWidget *parent, int bgOffsetTop, int bgOffsetBotto
     , m_bgOffsetTop(bgOffsetTop)
     , m_bgOffsetBottom(bgOffsetBottom)
 {
+    // 添加accessibleName
     this->setObjectName("DRoundBgFrame");
     this->setAccessibleName("DRoundBgFrame");
 }
 
+/**
+ * @brief DRoundBgFrame::paintEvent 重绘
+ * @param event
+ */
 void DRoundBgFrame::paintEvent(QPaintEvent *event)
 {
     Q_UNUSED(event)
 
     QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing, true);
+    painter.setRenderHint(QPainter::Antialiasing, true);            //设置边缘抗锯齿
     DPalette pa = DebApplicationHelper::instance()->palette(this);
 
     QPainterPath path;
     QRectF rect = this->rect();
-    rect.setX(0);
-    rect.setY(m_bgOffsetTop);
-    rect.setWidth(rect.width());
-    rect.setHeight(rect.height() - m_bgOffsetBottom);
+    rect.setX(0);                                                   //设置x位置
+    rect.setY(m_bgOffsetTop);                                       //设置y位置
+    rect.setWidth(rect.width());                                    //设置宽度
+    rect.setHeight(rect.height() - m_bgOffsetBottom);               //设置高度
 
     path.addRoundedRect(rect, 8, 8);
-    painter.fillPath(path, QBrush(pa.color(DPalette::Base)));
+    painter.fillPath(path, QBrush(pa.color(DPalette::Base)));       //填充颜色
 }
