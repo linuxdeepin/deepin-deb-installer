85bb2cb344f311dd58cacae975045aa5b1265e87
cuizhen <cuizhen@uniontech.com>
2020-09-27 18:06:09 +0800

    chore: 添加filechooseWidget代码注释
    添加filechooseWidget代码注释
    
    Log: 添加filechooseWidget代码注释
    Change-Id: Iba74cbb8aace58ac1a54cd18e81fd36e1f40595d
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/deepin-deb-installer/+/6461
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: liujun <liujuna@uniontech.com>
    Reviewed-by: cuizhen <cuizhen@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/deb_installer/filechoosewidget.cpp
+++ src/deb_installer/filechoosewidget.cpp
@@ -45,7 +45,9 @@ FileChooseWidget::FileChooseWidget(QWidget *parent)
     setAcceptDrops(true);
     DPalette palette;
 
+    // fileChooseWidget的图标
     m_iconImage = new DLabel(this);
+    //添加AccessibleName
     m_iconImage->setObjectName("iconImage");
     m_iconImage->setAccessibleName("iconImage");
     m_iconImage->setFixedSize(160, 160);
@@ -53,27 +55,41 @@ FileChooseWidget::FileChooseWidget(QWidget *parent)
 #ifdef SHOWBORDER
     iconImage->setStyleSheet("QLabel{border:1px solid black;}");
 #endif
+
+    // 拖入提示语
     m_dndTips = new DLabel(this);
     m_dndTips->setFixedHeight(30);
     m_dndTips->setAlignment(Qt::AlignTop);
     m_dndTips->setText(tr("Drag deb packages here"));
+
+    //添加AccessibleName
     m_dndTips->setObjectName("DNDTips");
 
+    //修改字体颜色
     palette = DApplicationHelper::instance()->palette(m_dndTips);
     palette.setBrush(DPalette::WindowText, palette.color(DPalette::TextTips));
     m_dndTips->setPalette(palette);
 
+    //修改字体大小与字体类型
     QString fontFamily = Utils::loadFontFamilyByType(Utils::SourceHanSansNormal);
     Utils::bindFontBySizeAndWeight(m_dndTips, fontFamily, 12, QFont::ExtraLight);
 
 #ifdef SHOWBORDER
     dndTips->setStyleSheet("QLabel{border:1px solid black;}");
 #endif
+
+    //分割线
     split_line = new DLabel(this);
+
+    //添加AccessibleName
     split_line->setObjectName("SplitLine");
     split_line->setAccessibleName("SplitLine");
+
+    //显示的大图标
     QIcon icon_install = QIcon::fromTheme("di_icon_install");
     m_iconImage->setPixmap(icon_install.pixmap(QSize(160, 160)));
+
+    //显示的分割线
     QIcon icon_split_line = QIcon::fromTheme("di_split_line");
     split_line->setPixmap(icon_split_line.pixmap(QSize(220, 3)));
     split_line->setFixedHeight(3);
@@ -82,6 +98,8 @@ FileChooseWidget::FileChooseWidget(QWidget *parent)
 //    m_chooseFileBtn = new ChooseFileButton(this);
     m_chooseFileBtn = new ChooseFileButton("", this);
     m_chooseFileBtn->setFixedHeight(28);
+
+    //添加AccessibleName
     m_chooseFileBtn->setObjectName("FileChooseButton");
     m_chooseFileBtn->setAccessibleName("FileChooseButton");
     m_chooseFileBtn->setText(tr("Select File"));
@@ -89,20 +107,30 @@ FileChooseWidget::FileChooseWidget(QWidget *parent)
     m_chooseFileBtn->setStyleSheet("QPushButton{border:1px solid black;}");
 #endif
 
+    //大图标的布局
     QVBoxLayout *centralLayout = new QVBoxLayout(this);
     centralLayout->addSpacing(32);
     centralLayout->addWidget(m_iconImage);
+    //设置图标居中
     centralLayout->setAlignment(m_iconImage, Qt::AlignHCenter);
 
+    //图标下是提示
     centralLayout->addSpacing(8);
     centralLayout->addWidget(m_dndTips);
+
+    //提示居中显示
     centralLayout->setAlignment(m_dndTips, Qt::AlignHCenter);
 
+    //分割线
     centralLayout->addWidget(split_line);
     centralLayout->setAlignment(split_line, Qt::AlignHCenter);
 
     centralLayout->addSpacing(11);
+
+    //文件选择按钮
     centralLayout->addWidget(m_chooseFileBtn);
+
+    //文件选择按钮居中显示
     centralLayout->setAlignment(m_chooseFileBtn, Qt::AlignHCenter);
     centralLayout->addStretch();
 
@@ -110,8 +138,11 @@ FileChooseWidget::FileChooseWidget(QWidget *parent)
     centralLayout->setContentsMargins(0, 0, 0, 0);
 
     setLayout(centralLayout);
+
+    //文件选择后，将数据传到chooseFiles信号中
     connect(m_chooseFileBtn, &ChooseFileButton::clicked, this, &FileChooseWidget::chooseFiles);
 
+    //主题变换时，修改图标的颜色
     QObject::connect(DGuiApplicationHelper::instance(), &DGuiApplicationHelper::themeTypeChanged,
                      this, &FileChooseWidget::themeChanged);
 }
@@ -121,33 +152,39 @@ FileChooseWidget::FileChooseWidget(QWidget *parent)
  */
 void FileChooseWidget::chooseFiles()
 {
-    QString historyDir = m_settings.value("history_dir").toString();
+    QString historyDir = m_settings.value("history_dir").toString();        //获取保存的文件路径
 
     if (historyDir.isEmpty()) {
         historyDir = QDir::homePath();
     }
 
-    DFileDialog dialog;
+    DFileDialog dialog;                                                     //获取FileDialog
     dialog.setFileMode(QFileDialog::ExistingFiles);
     dialog.setNameFilter("Debian Package Files (*.deb)");
-    dialog.setDirectory(historyDir);
+    dialog.setDirectory(historyDir);                                        //设置打开的路径为保存的路径
 
-    const int mode = dialog.exec();
+    const int mode = dialog.exec();                                         //打开文件选择窗口
 
     // save the directory string to config file.
-    m_settings.setValue("history_dir", dialog.directoryUrl().toLocalFile());
+    m_settings.setValue("history_dir", dialog.directoryUrl().toLocalFile());//保存当前文件路径
 
     if (mode != QDialog::Accepted) return;
 
-    const QStringList selected_files = dialog.selectedFiles();
+    const QStringList selected_files = dialog.selectedFiles();              //获取选中的文件
 
-    emit packagesSelected(selected_files);
+    emit packagesSelected(selected_files);                                  //发送信号
 }
 
+/**
+ * @brief FileChooseWidget::themeChanged 根据主题更新图标
+ */
 void FileChooseWidget::themeChanged()
 {
+    //更新icon
     QIcon icon_install = QIcon::fromTheme("di_icon_install");
     m_iconImage->setPixmap(icon_install.pixmap(QSize(160, 160)));
+
+    //更新 分割线
     QIcon icon_split_line = QIcon::fromTheme("di_split_line");
     split_line->setPixmap(icon_split_line.pixmap(QSize(220, 3)));
 }
@@ -158,6 +195,6 @@ void FileChooseWidget::themeChanged()
  */
 void FileChooseWidget::clearChooseFileBtnFocus()
 {
-    m_chooseFileBtn->clearFocus();
+    m_chooseFileBtn->clearFocus();                              //清除文件选择按钮的焦点
 }
 
--- src/deb_installer/filechoosewidget.h
+++ src/deb_installer/filechoosewidget.h
@@ -45,18 +45,28 @@ public:
     void clearChooseFileBtnFocus();
 
 signals:
+    /**
+     * @brief packagesSelected 选择文件信号
+     * @param files 通过文件选择窗口选择的文件
+     */
     void packagesSelected(const QStringList files) const;
 
 private slots:
+    /**
+     * @brief chooseFiles 打开文件选择窗口，选择文件后将文件保存
+     */
     void chooseFiles();
+    /**
+     * @brief themeChanged 更换主题时，修改UI
+     */
     void themeChanged();
 private:
-    ChooseFileButton *m_chooseFileBtn {nullptr};
-    QSettings m_settings;
-    DLabel *split_line {nullptr};
-    DLabel *m_dndTips {nullptr};
-    DLabel *m_iconImage {nullptr};
-    int m_MouseBtnRelease = 0;
+    ChooseFileButton *m_chooseFileBtn {nullptr};            //文件选择按钮
+    QSettings m_settings;                                   //保存上次打开的文件路径
+    DLabel *split_line {nullptr};                           //分割线
+    DLabel *m_dndTips {nullptr};                            //拖入提示语
+    DLabel *m_iconImage {nullptr};                          //图标
+    int m_MouseBtnRelease = 0;                              //此变量已废弃
 };
 
 #endif  // FILECHOOSEWIDGET_H
