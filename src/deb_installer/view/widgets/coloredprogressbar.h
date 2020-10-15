9a8c923e9094c81f2af2d3e07b5330b216636e9e
cuizhen <cuizhen@uniontech.com>
2020-10-15 17:22:45 +0800

    build: 服务器版qmake转cmake
    服务器版qmake转cmake
    
    Log: 服务器版qmake转cmake
    Change-Id: Ia32a621a28805841c32c49e39702abea185a3830
    Reviewed-on: http://gerrit.uniontech.com/c/nanjing/deepin-deb-installer/+/7660
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: chenhaifeng <chenhaifeng@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- src/deb_installer/view/widgets/coloredprogressbar.h
+++ src/deb_installer/view/widgets/coloredprogressbar.h
@@ -18,10 +18,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+#pragma once
 #include "dtkwidget_global.h"
-#include "dobject.h"
 #include "utils/utils.h"
+
+#include<DObject>
 
 #include <QProgressBar>
 
