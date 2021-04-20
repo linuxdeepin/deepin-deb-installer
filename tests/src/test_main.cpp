68318f0da56fd52f6ba13abc9f587b913332420a
cuizhen <cuizhen@uniontech.com>
2021-04-20 17:56:30 +0800

    chore: 安全性测试代码
    安全性测试代码
    
    Log: 安全性测试代码
    Change-Id: I3aa5a373fb5696e9810452e6b0253bca8df63881

--- tests/src/test_main.cpp
+++ tests/src/test_main.cpp
@@ -24,12 +24,20 @@ using namespace std;
 #include <limits.h>
 #include "gtest/gtest.h"
 #include <QApplication>
+
+#if defined(CMAKE_SAFETYTEST_ARG_ON)
+#include <sanitizer/asan_interface.h>
+#endif
+
 //  gtest的入口函数
 int main(int argc, char **argv) {
     qputenv("QT_QPA_PLATFORM","offscreen");
     QApplication a(argc,argv);
     ::testing::InitGoogleTest(&argc, argv);
     auto c= RUN_ALL_TESTS();
-    //Q_UNUSED(c);
+
+#if defined(CMAKE_SAFETYTEST_ARG_ON)
+    __sanitizer_set_report_path("asan.log");
+#endif
     return c;
 }
