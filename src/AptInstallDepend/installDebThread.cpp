b13fc603b3723d2d67c20e4dcd45121b047f4672
cuizhen <cuizhen@uniontech.com>
2021-04-26 13:23:50 +0800

    fix: 服务器企业版修复内容注入漏洞
    Description: 服务器企业版修复内容注入漏洞
    
    Log: 服务器企业版修复内容注入漏洞
    Change-Id: Ia7b01f0f41407b28d3f875d0301951d9a6e5fefc

--- src/AptInstallDepend/installDebThread.cpp
+++ src/AptInstallDepend/installDebThread.cpp
@@ -1,21 +1,19 @@
 /*
 * Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
 *
+* Author:     liupeng <liupeng@uniontech.com>
+* Maintainer: liupeng <liupeng@uniontech.com>
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * any later version.
-*
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
-*
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-
 #include "installDebThread.h"
 #include <QDebug>
 
@@ -25,13 +23,10 @@ InstallDebThread::InstallDebThread()
     connect(m_proc, SIGNAL(finished(int)), this, SLOT(onFinished(int)));
     connect(m_proc, SIGNAL(readyReadStandardOutput()), this, SLOT(on_readoutput()));
 }
-
 InstallDebThread::~InstallDebThread()
 {
-    if (m_proc != nullptr) {
+    if (m_proc)
         delete m_proc;
-        m_proc = nullptr;
-    }
 }
 
 void InstallDebThread::setParam(QStringList tParam)
@@ -39,9 +34,9 @@ void InstallDebThread::setParam(QStringList tParam)
     m_listParam = tParam;
 }
 
-void InstallDebThread::getDescription()
+void InstallDebThread::getDescription(QString debPath)
 {
-    QString str = "sudo dpkg -e " + m_listParam[1] + " " + TEMPLATE_DIR;
+    QString str = "dpkg -e " + debPath + " " + TEMPLATE_DIR;
     system(str.toUtf8());
 
     QFile file;
@@ -114,21 +109,110 @@ void InstallDebThread::run()
         } else if (m_listParam[0] == "InstallConfig") {
             if (m_listParam.size() <= 1)
                 return;
-
-            qDebug() << "StartInstallAptConfig";
-
-            getDescription();
-
-            //m_proc->start("sudo", QStringList() << "-S" <<  "dpkg-preconfigure" << "-f" << "Teletype" << m_listParam[1]);
-            m_proc->start("sudo", QStringList() << "-S" <<  "dpkg" << "-i" << m_listParam[1]);
-            m_proc->waitForFinished(-1);
-
-            QDir filePath(TEMPLATE_DIR);
-            if (filePath.exists()) {
-                filePath.removeRecursively();
+            const QFileInfo info(m_listParam[1]);
+            const QFile debFile(m_listParam[1]);
+            QString debPath = m_listParam[1];
+            if (debPath.contains(" ")
+                    || debPath.contains("&")
+                    || debPath.contains(";")
+                    || debPath.contains("|")) {
+                debPath = SymbolicLink(debPath, "installPackage");
             }
 
-            m_proc->close();
+            if (debFile.exists() && info.isFile() && info.suffix().toLower() == "deb") {        //大小写不敏感的判断是否为deb后缀
+                qDebug() << "StartInstallAptConfig";
+
+                getDescription(debPath);
+
+                //m_proc->start("sudo", QStringList() << "-S" <<  "dpkg-preconfigure" << "-f" << "Teletype" << m_listParam[1]);
+                m_proc->start("sudo", QStringList() << "-S" <<  "dpkg" << "-i" << debPath);
+                m_proc->waitForFinished(-1);
+
+                QDir filePath(TEMPLATE_DIR);
+                if (filePath.exists())
+                    filePath.removeRecursively();
+                m_proc->close();
+            }
+
         }
+    }
+}
+
+
+/**
+ * @brief PackagesManager::SymbolicLink 创建软连接
+ * @param previousName 原始路径
+ * @param packageName 软件包的包名
+ * @return 软链接的路径
+ */
+QString InstallDebThread::SymbolicLink(QString previousName, QString packageName)
+{
+    if (!mkTempDir()) {
+        qWarning() << "InstallDebThread:" << "Failed to create temporary folder";
+        return previousName;
+    }
+    return link(previousName, packageName);
+}
+
+/**
+ * @brief PackagesManager::mkTempDir 创建软链接存放的临时目录
+ * @return 创建目录的结果
+ */
+bool InstallDebThread::mkTempDir()
+{
+    QDir tempPath(m_tempLinkDir);
+    if (!tempPath.exists()) {
+        return tempPath.mkdir(m_tempLinkDir);
+    } else {
+        return true;
+    }
+}
+
+/**
+ * @brief PackagesManager::rmTempDir 删除存放软链接的临时目录
+ * @return 删除临时目录的结果
+ */
+bool InstallDebThread::rmTempDir()
+{
+    QDir tempPath(m_tempLinkDir);
+    if (tempPath.exists()) {
+        return tempPath.removeRecursively();
+    } else {
+        return true;
+    }
+}
+
+/**
+ * @brief PackagesManager::link 创建软链接
+ * @param linkPath              原文件的路径
+ * @param packageName           包的packageName
+ * @return                      软链接之后的路径
+ */
+QString InstallDebThread::link(QString linkPath, QString packageName)
+{
+    qDebug() << "InstallDebThread: Create soft link for" << packageName;
+    QFile linkDeb(linkPath);
+
+    //创建软链接时，如果当前临时目录中存在同名文件，即同一个名字的应用，考虑到版本可能有变化，将后续添加进入的包重命名为{packageName}_1
+    //删除后再次添加会在临时文件的后面添加_1,此问题不影响安装。如果有问题，后续再行修改。
+    int count = 1;
+    QString tempName = packageName;
+    while (true) {
+        QFile tempLinkPath(m_tempLinkDir + tempName);
+        if (tempLinkPath.exists()) {
+            tempName = packageName + "_" + QString::number(count);
+            qWarning() << "InstallDebThread:" << "A file with the same name exists in the current temporary directory,"
+                       "and the current file name is changed to"
+                       << tempName;
+            count++;
+        } else {
+            break;
+        }
+    }
+    if (linkDeb.link(linkPath, m_tempLinkDir + tempName))
+        return m_tempLinkDir + tempName;
+    else {
+        qWarning() << "InstallDebThread:" << "Failed to create Symbolick link error.";
+        return linkPath;
     }
 }
--- src/AptInstallDepend/installDebThread.h
+++ src/AptInstallDepend/installDebThread.h
@@ -1,20 +1,19 @@
 /*
-* Copyright (C) 2019 ~ 2020 Uniontech Software Technology Co.,Ltd.
+* Copyright (C) 2019 ~ 2020 Deepin Technology Co., Ltd.
 *
+* Author:     liupeng <liupeng@uniontech.com>
+* Maintainer: liupeng <liupeng@uniontech.com>
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * any later version.
-*
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
-*
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 #ifndef INSTALLDEBTHREAD_H
 #define INSTALLDEBTHREAD_H
 
@@ -34,7 +33,7 @@ public:
     InstallDebThread();
     virtual ~InstallDebThread();
     void setParam(QStringList tParam);
-    void getDescription();
+    void getDescription(QString debPath);
     void run();
     int m_resultFlag = -1;
 
@@ -46,5 +45,15 @@ private:
     QProcess *m_proc;
     QStringList m_listParam;
     QList<QString> m_listDescribeData;
+
+private:
+
+    const QString m_tempLinkDir = "/tmp/LinkTemp/";
+    //使用软连接方式解决文件路径中存在空格的问题。
+    QString SymbolicLink(QString previousName, QString packageName);
+    QString link(QString linkPath, QString packageName);
+    bool mkTempDir();
+    bool rmTempDir();
+
 };
 #endif // INSTALLDEBTHREAD_H
