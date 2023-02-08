4b03d5af1fca1c9668a868750ad0f44c4210ed03
wangzhengyang <wangzhengyang@uniontech.com>
2023-02-08 10:26:04 +0800

    feat: 全面更新翻译文件
    全面更新翻译文件
    
    Log: 全面更新翻译文件

---
+++ translations/desktop/desktop_fil.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fil" version="2.1"><context>
+<name>desktop</name>
+<message>
+    <location filename="Desktop Entry]Name" line="0"/>
+    <source>Deepin Package Installer</source><translation>Deepin Package Installer</translation>
+</message>
+<message>
+    <location filename="Desktop Entry]GenericName" line="0"/>
+    <source>Package Installer</source><translation>Package Installer</translation>
+</message>
+<message>
+    <location filename="Desktop Entry]Comment" line="0"/>
+    <source>Package Installer helps users install and remove local packages, and supports bulk installation.</source>
+    <translation>Ang Package Installer ay nakakatulog sa pag install at pag-tanggal ng mga local packages, at sinusuportahan ito nang pag-install ng mga packages na maramihan.</translation>
+</message>
+</context>
+</TS>
--- translations/policy/policy_fil.ts
+++ translations/policy/policy_fil.ts
@@ -1,15 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="pl" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fil" version="2.1">
 	<context>
 		<name>policy</name>
 		<message>
 			<location filename="com.deepin.pkexec.deepin-deb-installer-dependsInstall!message" line="0"/>
 			<source>Authentication is required to continue the configuration</source>
-			<translation>Aby kontynuować konfigurację, wymagane jest uwierzytelnienie</translation>
+			<translation>Authentication ay kailangan upang magpatuloy sa pag-ayos</translation>
 		</message>
 		<message>
 			<location filename="com.deepin.pkexec.deepin-deb-installer-dependsInstall!description" line="0"/>
 			<source>authentication</source>
-			<translation>uwierzytelnienie</translation>
+			<translation>authentication</translation>
 		</message>
 	</context>
 </TS>
