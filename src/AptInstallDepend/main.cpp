c7164dcdc1f3c1fbc97f9017c55ce304fa9bdfd2
wangzhengyang <wangzhengyang@uniontech.com>
2023-02-17 10:26:35 +0800

    fix: wine应用依赖安装添加调用方校验
    wine应用依赖安装添加调用方校验
    
    Log: wine应用依赖安装添加调用方校验
    Bug: https://pms.uniontech.com/bug-view-187177.html

--- src/AptInstallDepend/main.cpp
+++ src/AptInstallDepend/main.cpp
@@ -10,12 +10,52 @@
 #include <QString>
 #include <QStringList>
 #include <QThread>
+#include <QStandardPaths>
 #include <iostream>
 #include "installDebThread.h"
+
+#include <sys/types.h>
+#include <unistd.h>
+
+bool isValidInvoker()
+{
+    bool valid = false;
+
+    //判断是否存在执行路径
+    pid_t pid = getppid();
+    QFileInfo f(QString("/proc/%1/exe").arg(pid));
+    if (!f.exists()) {
+        valid = false;
+    } else {
+        valid = true;
+    }
+
+    //是否存在于可调用者名单中
+    QStringList ValidInvokerExePathList;
+    QString invokerPath = f.canonicalFilePath();
+    QStringList findPaths;//合法调用者查找目录列表
+    findPaths << "/usr/bin";
+    ValidInvokerExePathList << QStandardPaths::findExecutable("deepin-deb-installer", findPaths);
+
+    if (valid) {
+        valid = ValidInvokerExePathList.contains(invokerPath);
+    }
+
+    //非法调用
+    if (!valid) {
+        qWarning() << QString("(pid: %1)[%2] is not allowed to configrate firewall").arg(pid).arg((invokerPath));
+        return false;
+    }
+    return true;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication app(argc, argv);
+
+    if (!isValidInvoker()) {
+        return 1;
+    }
 
     QCommandLineParser parser;
     parser.process(app);
