5a5108fe89267b7988179e262e519966f856debc
renbin <renbin@uniontech.com>
2023-05-23 15:20:37 +0800

    feat: 添加安全等级设置引导提示
    添加安全等级设置引导提示，分级管控
    提示调整为安装完成后统一提示。
    在设置菜单中添加跳转链接，屏蔽旧版配置。
    
    Log: 添加安全等级设置引导提示
    Task: https://pms.uniontech.com/story-view-28999.html
    Influence: 分级管控

--- src/deb-installer/view/pages/settingdialog.cpp
+++ src/deb-installer/view/pages/settingdialog.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2022-2023 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
@@ -8,6 +8,8 @@
 #include <DSettingsWidgetFactory>
 #include <DStandardPaths>
 #include <DCheckBox>
+#include <DLabel>
+#include <DGuiApplicationHelper>
 
 #include <QApplication>
 #include <QDir>
@@ -15,6 +17,78 @@
 #include <QDebug>
 #include <QTextStream>
 #include <QSettings>
+
+// 富文本链接模板 color: #0082FA
+const char WEBSITE_LINK_TEMPLATE[] = "<a href='%1' style='text-decoration: none; font-size:%2px; color: %3;'>%4</a>";
+
+/**
+   @class ProceedLabel
+   @brief 分级管控安全等级设置引导提示控件
+ */
+class ProceedLabel : public DLabel
+{
+public:
+    explicit ProceedLabel();
+    ~ProceedLabel() override = default;
+
+protected:
+    bool event(QEvent *e) override;
+    void updateText();
+};
+
+ProceedLabel::ProceedLabel()
+{
+    setObjectName("OptionProceedLabel");
+    setAccessibleName("OptionProceedLabel");
+
+    setWordWrap(true);
+    setContextMenuPolicy(Qt::NoContextMenu);
+    setOpenExternalLinks(false);
+
+    updateText();
+
+    // 点击链接跳转安全中 > 应用安全
+    QObject::connect(
+        this, &DLabel::linkActivated, [](const QString &) { HierarchicalVerify::instance()->proceedDefenderSafetyPage(); });
+}
+
+/**
+   @reimp
+ */
+bool ProceedLabel::event(QEvent *e)
+{
+    switch (e->type()) {
+        case QEvent::PaletteChange:
+            updateText();
+            break;
+        default:
+            break;
+    }
+
+    return DLabel::event(e);
+}
+
+/**
+   @brief 更新提示的文件的颜色，Dark模式下，显示文字可能偏暗，如有必要，可通过如下代码调整亮度凸显效果。
+   @code
+    if (DGuiApplicationHelper::DarkType == DGuiApplicationHelper::instance()->themeType()) {
+        color = color.lighter(150 / color.lightness() * 100);
+    }
+   @endcode
+ */
+void ProceedLabel::updateText()
+{
+    QString proceedNotify =
+        QObject::tr("To install unsigned apps, go to Security Center > Tools > App Security, and select the app types that can "
+                    "be installed.");
+    QString tmp = QObject::tr("Security Center > Tools > App Security");
+    QColor color = palette().color(QPalette::Highlight);
+    int pxSize = font().pixelSize();
+
+    proceedNotify =
+        proceedNotify.replace(tmp, QString(WEBSITE_LINK_TEMPLATE).arg("localtest.com").arg(pxSize).arg(color.name()).arg(tmp));
+    setText(proceedNotify);
+}
 
 SettingDialog::SettingDialog(QWidget *parent)
     : DSettingsDialog(parent)
@@ -29,9 +103,8 @@ SettingDialog::~SettingDialog()
 
 void SettingDialog::init()
 {
-    // 设置默认的校验勾选框
     if (widgetFactory()) {
-        widgetFactory()->registerWidget("verifycheckbox", &SettingDialog::createVerifyCheckBox);
+        widgetFactory()->registerWidget("proceedLabel", &SettingDialog::createProceedDefenderSafetyLabel);
     }
 
     m_setting = DSettings::fromJsonFile(":/data/deepin-deb-installer.json");
@@ -59,6 +132,10 @@ void SettingDialog::init()
         m_isDigital = m_setting->value("basic.develop_digital_verify.").toBool();
     });
     m_isDigital = m_setting->value("basic.develop_digital_verify.").toBool();
+
+    // 分级管控不同状态下切换显示信息，初始化后绑定信号
+    switchHierarchicalNotify(HierarchicalVerify::instance()->isValid());
+    connect(HierarchicalVerify::instance(), &HierarchicalVerify::validChanged, this, &SettingDialog::switchHierarchicalNotify);
 }
 
 bool SettingDialog::isDigitalVerified()
@@ -67,39 +144,14 @@ bool SettingDialog::isDigitalVerified()
 }
 
 /**
-   @return 根据传入的设置参数 \a obj 创建用于配置校验信息的勾选框，
-    存在分级管控接口时，静止配置验签功能，选项置灰。
+   @brief 创建前往安全中心的跳转链接，弹出分级管控安全等级设置引导提示窗口
+   @return 创建的跳转提示控件
  */
-QWidget *SettingDialog::createVerifyCheckBox(QObject *obj)
+QWidget *SettingDialog::createProceedDefenderSafetyLabel(QObject *obj)
 {
-    auto translateContext = obj->property("_d_DSettingsWidgetFactory_translateContext").toByteArray();
-    auto option = qobject_cast<DTK_CORE_NAMESPACE::DSettingsOption *>(obj);
-    auto value = option->data("text").toString();
-    QString trName;
-    if (translateContext.isEmpty()) {
-        trName = QObject::tr(value.toStdString().c_str());
-    } else {
-        trName = qApp->translate(translateContext, value.toStdString().c_str());
-    }
-    DCheckBox *checkBox = new DCheckBox(trName);
-
-    checkBox->setObjectName("OptionVerifyCheckbox");
-    checkBox->setAccessibleName("OptionVerifyCheckbox");
-
-    // 分级管控模式下，不允许设置安装器验签
-    checkBox->setEnabled(!HierarchicalVerify::instance()->isValid());
-    QObject::connect(HierarchicalVerify::instance(), &HierarchicalVerify::validChanged, checkBox, [=](bool valid) {
-        checkBox->setEnabled(!valid);
-    });
-
-    checkBox->setChecked(option->value().toBool());
-    option->connect(checkBox, &QCheckBox::stateChanged, option, [=](int status) { option->setValue(status == Qt::Checked); });
-    option->connect(option, &DTK_CORE_NAMESPACE::DSettingsOption::valueChanged, checkBox, [=](const QVariant &value) {
-        checkBox->setChecked(value.toBool());
-        checkBox->update();
-    });
-
-    return checkBox;
+    Q_UNUSED(obj)
+    ProceedLabel *label = new ProceedLabel;
+    return label;
 }
 
 /**
@@ -110,4 +162,14 @@ void SettingDialog::showEvent(QShowEvent *e)
     // 刷新分级管控接口状态
     (void)HierarchicalVerify::instance()->isValid();
     DSettingsDialog::showEvent(e);
+}
+
+/**
+   @brief 跟进 @a valid 当前是否处于分级管控状态切换显示界面
+ */
+void SettingDialog::switchHierarchicalNotify(bool valid)
+{
+    setGroupVisible("basic.develop_digital_verify", !valid);
+    setResetVisible(!valid);
+    setGroupVisible("basic.hierarachical_verify", valid);
 }
