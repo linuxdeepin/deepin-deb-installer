5788f99b9513b6448291169ece51cfa2229835a3
LiHua000 <zhangtingan@uniontech.com>
2025-05-07 17:26:12 +0800

    fix: incorrect display of menu (#331)
    Log: as title
    
    Bug: https://pms.uniontech.com/bug-view-315501.html

--- src/deb-installer/view/pages/debinstaller.cpp
+++ src/deb-installer/view/pages/debinstaller.cpp
@@ -118,7 +118,6 @@ void DebInstaller::initTitleBar()
     DTitlebar *tb = titlebar();
     if (tb != nullptr) {
         tb->setMenu(menu);
-        menu->setParent(tb);
         tb->setIcon(QIcon::fromTheme("deepin-deb-installer"));
         tb->setTitle("");
         tb->setAutoFillBackground(true);
