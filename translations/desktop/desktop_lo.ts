b76e2b11b8267c06f62e749c58d25c16477d8138
JWWTSL <167396168+JWWTSL@users.noreply.github.com>
2025-08-07 20:12:52 +0800

    feat: Update translations for Lao locale (#350)
    log: Translation update

---
+++ translations/desktop/desktop_lo.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="lo">
+<context>
+    <name>desktop</name>
+    <message>
+        <location filename="Desktop Entry]Name" line="0"/>
+        <source>Deepin Package Installer</source>
+        <translation>ລូຫ្គិន ກියិតសຳຫຼື ກຳນຳ ອົງປະກອບ</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]GenericName" line="0"/>
+        <source>Package Installer</source>
+        <translation>ກຳນຳ ອົງປະກອບ</translation>
+    </message>
+    <message>
+        <location filename="Desktop Entry]Comment" line="0"/>
+        <source>Package Installer helps users install and remove local packages, and supports bulk installation.</source>
+        <translation>ກຳນຳ ອົງປະກອບ ກຳນຳຜູ້ໃຊ້ງານ ກຳນຳ ອົງປະກອບທ່ານຳໃນທະນາແມງແລະ ຂ້ອງກຳນຳກຳກຳກຳ ກຳນຳພາກຳພັນທະນາກຳນຳພາກຳໃຫ້ມີກວ່າໜີ້ໜີ້. ມີສະຫຼີດການນຳໃຊ້ກຳນຳພາກຳພັນທະນາ.</translation>
+    </message>
+</context>
+</TS>
